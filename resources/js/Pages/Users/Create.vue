fd963f355d3e4a10676676edbd6216ca702f30ab
x-ahmed <appsxapp@gmail.com>
2021-06-27 10:43:36 +0200

    showed user create form validation errors

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,48 +1,55 @@
 <template>
     <layout>
-        <div class="container">
-            <div class="row justify-content-center">
-                <form
-                    class="col-md-8 my-5"
-                    action="/users"
-                    method="POST"
-                    @click.prevent="submit"
-                >
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            placeholder="your name"
-                            v-model="form.name"
-                        />
+        <div class="row justify-content-center">
+            <form
+                class="col-md-8 my-5"
+                action="/users"
+                method="POST"
+                @click.prevent="submit"
+            >
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="name"
+                        placeholder="your name"
+                        v-model="form.name"
+                    />
+                    <div v-if="errors.name">
+                        <span class="text-danger">{{ errors.name }}</span>
                     </div>
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input
-                            type="email"
-                            class="form-control"
-                            id="email"
-                            placeholder="valid email address"
-                            v-model="form.email"
-                        />
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        id="email"
+                        placeholder="valid email address"
+                        v-model="form.email"
+                    />
+                    <div v-if="errors.email">
+                        <span class="text-danger">{{ errors.email }}</span>
                     </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                            type="password"
-                            class="form-control"
-                            id="password"
-                            placeholder="secured password"
-                            v-model="form.password"
-                        />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="password"
+                        placeholder="secured password"
+                        v-model="form.password"
+                    />
+                    <div v-if="errors.password">
+                        <span class="text-danger">{{ errors.password }}</span>
                     </div>
-                    <button type="submit" class="btn btn-primary">
-                        Create
-                    </button>
-                </form>
-            </div>
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    Create
+                </button>
+            </form>
         </div>
     </layout>
 </template>
@@ -51,26 +58,25 @@
 import Layout from "../Shared/Layout";
 
 export default {
-    props: [],
+    props: {
+        errors: Object
+    },
     components: {
         Layout
     },
     data: () => {
         return {
             form: {
-                name: '',
-                email: '',
-                password: '',
-            },
-        }
+                name: "",
+                email: "",
+                password: ""
+            }
+        };
     },
     methods: {
-        submit(){
-            this.$inertia.post('/users', this.form)
-                .then(() => {
-                    // code
-                })
-        },
+        submit() {
+            this.$inertia.post("/users", this.form);
+        }
     }
 };
 </script>
